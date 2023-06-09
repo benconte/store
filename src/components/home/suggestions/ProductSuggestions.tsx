6dfef9a231a4f657d6a31ce35095d42e1ae42e3d
benconte <benconte201@gmail.com>
2023-06-09 18:47:24 +0200

    refactored productSugestions

--- src/components/home/suggestions/ProductSuggestions.tsx
+++ src/components/home/suggestions/ProductSuggestions.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react'
 
 const ProductSuggestions = ({ suggestedProducts }: { suggestedProducts: ProductSuggestions[] }) => {
     return (
-        <div className='w-full mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        <div className='w-full mt-6 md:mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
             {suggestedProducts.map((product) => (
                 <div className={clsx('flex flex-col justify-center rounded-lg px-4 pt-6', product.background !== 'bg-black' ? `bg-white` : 'bg-black')} key={product.productType}>
                     <small
@@ -34,7 +34,7 @@ const ProductSuggestions = ({ suggestedProducts }: { suggestedProducts: ProductS
                             src={product.image}
                             alt={product.title}
                             fill
-                            className='w-full h-full absolute top-1/3 left-0 right-0 -translate-2/4 object-cover object-bottom'
+                            className='w-full h-full md:absolute md:top-1/3 md:left-0 md:right-0 md:-translate-2/4 object-contain md:object-cover object-bottom'
                         />
                     </div>
                 </div>
