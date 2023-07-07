617fad45756c07401cb6bf863d2174c95a7f6d41
benconte <benconte201@gmail.com>
2023-07-07 18:32:21 +0200

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
-import { withAuth } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  pages: {
-    signIn: "/auth",
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware() {
+    return NextResponse.next()
   },
-});
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: "/auth"
+    }
+  },
+)
+
 
 export const config = { 
   matcher: [
