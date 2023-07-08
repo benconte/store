d4775eae40c8e435d72f618a0fa5fd7f4838573c
benconte <benconte201@gmail.com>
2023-07-08 17:54:47 +0200

    refactored orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -14,7 +14,7 @@ const page = async () => {
   return (
     <div className="w-full px-2 md:px-7 lg:px-12 py-5 md:py-8">
       {orders.length > 0 ?
-        <div className='w-full h-full px-2 md:px-7 lg:px-12 py-5 md:py-8  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5'>
           {orders?.map((prod) => (
             <OrderedProduct product={prod.product} key={prod.id} />
           ))}
